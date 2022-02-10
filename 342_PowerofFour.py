260b404684c7e8ef9be1306dbc5f46851fa90f27
DevYJShin <yjshin.dev@gmail.com>
2022-02-11 01:07:58 +0900

    second commit

--- 342_PowerofFour.py
+++ 342_PowerofFour.py
@@ -1,11 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n <= 0:
-            return False
-        else:
-            while n%4 == 0:
-                n /= 4
-            if n == 1:
-                return True
-            else:
-                return False
+        return math.log(n, 1/4) % 1 == 0 if n > 0 else False
